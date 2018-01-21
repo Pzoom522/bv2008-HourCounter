a2f130833569ff4515df097d53aa957f05120240
Pzoom <admin@pzoomspace.cn>
2018-01-22 02:46:10 +0800

    睡觉去

---
+++ pages/core.py
@@ -0,0 +1,35 @@
+#encoding="utf-8"
+from base64 import b64encode
+from Crypto.Cipher import PKCS1_v1_5 
+from Crypto.PublicKey import RSA
+from requests import session
+
+key_bytes=b"-----BEGIN PUBLIC KEY-----\n\
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbJ2QYNdiFlzE0mcyq7tcZc5dP\n\
+vof6696l2cJJM8kOxeXT8EonfvLzfsEGmwjNp3gvAyF14LvqT6w7oH40sFFnX358\n\
+Eb+HZXx6CZ4LOkaTW0KNS6yodsRv0uwJhFMwREqEVbqd6jcCxTGKDOieendC8x1f\n\
+sg3Muagyfawc+o+tewIDAQAB\n\
+-----END PUBLIC KEY-----"
+#生成Crypto.PublicKey.RSA._RSAobj类型的对象
+publickey=RSA.importKey(key_bytes)
+#构造“加密器”
+encryptor=PKCS1_v1_5.new(publickey)
+#加密的内容必须为bytes类型
+username=b'buaa1106'
+password=b'scse1506'
+#编码规范化&加密，并将结果转换成字符串
+input1=str(username,'utf-8')
+input2=str(b64encode(encryptor.encrypt(password)),'utf-8')
+
+payload = {
+    'uname': input1,
+    'upass': input2
+}
+
+target_file=open("e://target.html",mode="w",encoding="utf-8")
+with session() as c:
+    response=c.post('http://www.bv2008.cn/app/user/login.php?m=login',payload)
+    response = c.get('http://www.bv2008.cn/app/sys/view.vol.php?type=hour&uid=15195276&p=3')
+    #target_file.write(str(response.headers))
+    target_file.write(str(response.text))
+target_file.close()
--- src/counter.py
+++ src/counter.py
@@ -1,11 +1,35 @@
-file=open("C://Users/Pzoom/Documents/GitHub/bv2008-HourCounter/pages/target.html",mode='r',encoding='utf-8')
+class targetUser:
+    def __init__(self,uid,sessionName,startDate,endDate):
+        self.id=int(uid)#志愿北京编号
+        self.s=sessionName
+        self.start=startDate
+        self.end  =  endDate
+        self.total=0#总时长
 
+    def get_page(self,pageNo):
+        url='http://www.bv2008.cn/app/sys/view.vol.php?type=hour&uid='+self.id+'&p='+int(pageNo)
+        response = self.s.get(url)
+
+    def work(self):
+        initPage=0
+        while self.perPage()!=0:
+            
+        
+    file=open("e://target.html",mode='r',encoding='utf-8')
+total=0
+startDate=20160101
+endDate=20160607
 lineList = file.readlines()
 startIndex=lineList.index('<tr><th>服务项目</th><th width="80">服务时长</th><th width="80">添加方式</th><th width="80">状态</th><th width="150">日期</th></tr>\n')+1
 counter=0
 while '<a target="_blank" href="/app/opp/view.php?id=' in lineList[startIndex+counter*9+2]:
+    #单个项目
     hour=lineList[startIndex+counter*9+4]
-    valid=lineList[startIndex+counter*9+6]
+    hour=hour[4:len(hour)-6]#时长，一位小数
+    valid=lineList[startIndex+counter*9+6]#状态，{已生效,}
     time=lineList[startIndex+counter*9+7]
-    print('【'+hour+valid+time+'】\n')
+    time=time[4:14].replace('-','')#日期，YYYYMMDD
+    if (int(time)>=startDate and int(time)<=endDate) and ('已生效' in valid):
+        total=total+float(hour)
     counter=counter+1
+print(total)
